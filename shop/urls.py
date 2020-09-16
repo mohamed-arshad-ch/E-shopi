17ebb0faea8db611d099b7ecb7d9908a685f9731
mohamed arshad ch <mohamedarshadcholasseri5050@gmail.com>
2020-09-16 11:33:04 +0530

    user url set

--- shop/urls.py
+++ shop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from user import urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,17 @@
-from django.contrib import admin
-from django.urls import path,include
-from user import urls
-from views import landingpage
+
+from django.urls import path
+from .import views
 
 urlpatterns = [
     path('',views.landingpage,name="landingpage"),
+    path('shop-grid',views.landingpage,name="shopgrid"),
+    path('cart',views.landingpage,name="cart"),
+    path('checkout',views.landingpage,name="checkout"),
+    path('contact',views.landingpage,name="contact"),
+    path('product-details',views.landingpage,name="productdetails"),
+    path('wishlist',views.landingpage,name="wishlist"),
+    path('checkout-details-add',views.landingpage,name="checkoutdetailsadd"),
+    path('login',views.landingpage,name="login"),
+    path('register',views.landingpage,name="register"),
     
 ]
